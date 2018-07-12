3c5310c6bbccb595606b40b073463abc6d7a76de
Sami <sm.ams37@gmail.com>
2018-07-12 13:31:41 +0500

    initial commit with procfile public database v1

--- config/database.php
+++ config/database.php
@@ -74,15 +74,13 @@ return [
 
         'pgsql_production' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $user_name,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
-            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
