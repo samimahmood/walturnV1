4806984c2164b8131aa3dbfb32c77cdb4563d37c
Sami <sm.ams37@gmail.com>
2018-07-12 12:58:24 +0500

    initial commit with procfile public appservice

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,20 @@
 
 namespace App\Providers;
 
+use Illuminate\Contracts\Routing\UrlGenerator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
+
+
+    public function boot(UrlGenerator $url)
     {
+        if(env('REDIRECT_HTTPS')) {
+            $url->formatScheme('https');
+        }
+
         Schema::defaultStringLength(191);
 
     }
@@ -25,6 +27,28 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        if(env('REDIRECT_HTTPS')) {
+            $this->app['request']->server->set('HTTPS', true);
+        }
     }
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+//    public function boot()
+//    {
+//        Schema::defaultStringLength(191);
+//
+//    }
+//
+//    /**
+//     * Register any application services.
+//     *
+//     * @return void
+//     */
+//    public function register()
+//    {
+//        //
+//    }
 }
